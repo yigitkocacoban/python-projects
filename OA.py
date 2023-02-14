c9dc90a00a4b0c0413f66526ebf339d2b5c5a306
yigitkocacoban <66291474+yigitkocacoban@users.noreply.github.com>
2023-02-14 07:35:03 +0300

    Add files via upload

---
+++ OA.py
@@ -0,0 +1,27 @@
+# Yapay Zeka'ya giriş
+
+print("Hello World!")
+
+# Büyük harf ile başlanmaz
+# Sayı ile başlanmaz
+
+sayiToplam = 50
+
+sayi1 = 15
+
+sayi2 = 5.4
+
+sayi3 = 5.0
+
+print(type(sayi1))
+
+print(type(sayi2))
+
+print(type(sayi3))
+
+yazi = 'yazi'
+
+print(type(yazi))
+
+# int() noktadan sonrasında yazan sayıyı almaz
+
